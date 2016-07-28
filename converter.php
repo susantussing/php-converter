86535379a6065792fb4c5f2526118efba2f25c4b
Susan Tussing <susan@fionavar.net>
2016-07-28 11:23:26 -0500

    Add temperature converter class

--- converter.php
+++ converter.php
@@ -18,4 +18,17 @@ class Converter {
   }
 }
 
+class Temperature extends Converter {
+  function __construct($amount, $unit) {
+    parent::__construct($amount, $unit);
+    $this->base_unit = "f";
+    if ($unit == $this->base_unit) {
+      $this->base_amount = $amount;
+    } else if ($unit == "c") {
+      $this->base_amount = $amount * 1.8 + 32;
+    }
+    $this->conversions["c"] = "(amount - 32) / 1.8";
+  }
+}
+
 ?>
